390e308860a9a85ff5eb659fedb0200778305320
Joseph <Jbonventre95@gmail.com>
2023-01-25 11:18:17 -0500

    DB controller layout for restaurants

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/restaurants" do
+    restaurants = Restaurant.all
+    restaurants.to_json(include: { reviews: { include: :customer }})
+    end
+
+    get "/restaurants/:id" do 
+      restaurant = Restaurant.find(params[:id])
+      restaurant.to_json(include: { reviews: { include: :customer }})
+    end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ActiveRecord::Base
 has_many :reviews
-has_many :users, through: :reviews
+has_many :customers, through: :reviews
 end  
