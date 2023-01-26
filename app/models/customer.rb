fae7e78a801fefff0f7259b88039363a2f45e09b
James Cogan <james.m.cogan@gmail.com>
2023-01-26 11:47:21 -0500

    res images

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,13 @@
 class Customer < ActiveRecord::Base
 has_many :reviews
 has_many :restaurants, through: :reviews
+
+    def full_name
+     "#{first_name} #{last_name}"
+    end
+
+    def favorite_restaurant
+        binding.pry
+        restaurant.star_rating.max
+    end
 end
